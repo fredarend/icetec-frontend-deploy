663be2cc5e773262d2cee247471e10b5b474268a
unknown <fredericobinsfeld@gmail.com>
2020-09-26 19:39:24 -0300

    Update server url

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('@Icetec:token');
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://appdevelopers.fredericobinsfeld.com.br',
 });
 
 const config = {
